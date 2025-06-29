549b5b895a2b7c2d0b3ac2b55d99b6d4dd882f3c
HaiderGanai <ganaiihaider07@gmail.com>
2025-06-29 22:50:06 +0500

    Update dbConnect.js

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -6,6 +6,13 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
+     dialect: process.env.DB_DIALECT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Set false for self-signed certs
+      },
+    },
     logging: false,
 });
 
