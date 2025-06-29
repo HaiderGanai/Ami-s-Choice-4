cf56fce33bfa411ccb62e2875f59ab74ce3feff3
HaiderGanai <ganaiihaider07@gmail.com>
2025-06-29 23:02:13 +0500

    Update dbConnect.js

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -3,8 +3,8 @@ const { fn, col, Sequelize } = require('sequelize');
 
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST
-    dialect: process.env.DB_DIALECT
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
     logging: false,
 });
 
