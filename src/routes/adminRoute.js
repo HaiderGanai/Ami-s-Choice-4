8f30f0c204ab9b26b331a435030ed01c5c5ef685
Haider Ganai <ganaiihaider07@gmail.com>
2026-05-15 21:35:27 +0500

    feat: wire up all admin routes under /api/v1/admin/

--- src/routes/adminRoute.js
+++ src/routes/adminRoute.js
@@ -1,8 +1,73 @@
 const express = require('express');
 const adminRouter = express.Router();
+const { isAdmin } = require('../middlewares/isAdminMiddleware');
+const upload = require('../middlewares/upload');
 
-const { login } = require('../controllers/adminController');
+const {
+  login,
+  getAllUsers,
+  getStats,
+  adminGetAllProducts,
+  adminCreateProduct,
+  adminBulkCreateProducts,
+  adminUpdateProduct,
+  adminUpdateProductStatus,
+  adminDeleteProduct,
+  adminGetAllCategories,
+  adminCreateCategory,
+  adminUpdateCategory,
+  adminUpdateCategoryStatus,
+  adminGetAllOrders,
+  adminGetOrderDetail,
+  adminUpdateOrderStatus,
+  adminListCoupons,
+  adminCreateCoupon,
+  adminUpdateCoupon,
+  adminDeleteCoupon,
+  adminGetAllReviews,
+  adminDeleteReview,
+  adminGetSupportForms,
+} = require('../controllers/adminController');
 
+// Auth
 adminRouter.post('/login', login);
+
+// Dashboard
+adminRouter.get('/stats', isAdmin, getStats);
+
+// Users
+adminRouter.get('/users', isAdmin, getAllUsers);
+
+// Products
+adminRouter.get('/products', isAdmin, adminGetAllProducts);
+adminRouter.post('/products', isAdmin, upload.single('image'), adminCreateProduct);
+adminRouter.post('/bulk-products', isAdmin, adminBulkCreateProducts);
+adminRouter.put('/products/:id', isAdmin, upload.single('image'), adminUpdateProduct);
+adminRouter.patch('/products/:id/status', isAdmin, adminUpdateProductStatus);
+adminRouter.delete('/products/:id', isAdmin, adminDeleteProduct);
+
+// Categories
+adminRouter.get('/categories', isAdmin, adminGetAllCategories);
+adminRouter.post('/categories', isAdmin, adminCreateCategory);
+adminRouter.put('/categories/:id', isAdmin, adminUpdateCategory);
+adminRouter.patch('/categories/:id/status', isAdmin, adminUpdateCategoryStatus);
+
+// Orders
+adminRouter.get('/orders', isAdmin, adminGetAllOrders);
+adminRouter.get('/orders/:orderNumber', isAdmin, adminGetOrderDetail);
+adminRouter.patch('/orders/:orderNumber/status', isAdmin, adminUpdateOrderStatus);
+
+// Coupons
+adminRouter.get('/coupons', isAdmin, adminListCoupons);
+adminRouter.post('/coupons', isAdmin, adminCreateCoupon);
+adminRouter.patch('/coupons/:code', isAdmin, adminUpdateCoupon);
+adminRouter.delete('/coupons/:code', isAdmin, adminDeleteCoupon);
+
+// Reviews
+adminRouter.get('/reviews', isAdmin, adminGetAllReviews);
+adminRouter.delete('/reviews/:id', isAdmin, adminDeleteReview);
+
+// Support Forms
+adminRouter.get('/support-forms', isAdmin, adminGetSupportForms);
 
 module.exports = { adminRouter };
