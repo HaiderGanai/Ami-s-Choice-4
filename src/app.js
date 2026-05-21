5c1fd6bfc553c82401771a8a90f6991792205272
Haider Ganai <ganaiihaider07@gmail.com>
2026-05-21 14:20:16 +0500

    changed syn to alter true

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(cors());
 require('./associations');
 app.use('/uploads', express.static('uploads'));
 
-sequelize.sync({ force: true });
+sequelize.sync({ alter: true });
 
 loadRoutes(app);
 
